601aeda2d4b9e3c71c846735adc5e05aeacb2e18
Rick Hallett <rickhallett@icloud.com>
2025-06-30 11:21:33 +0100

    feat(starlink-optimiser): setup project structure

---
+++ StarLinkOptimiser/src/index.ts
@@ -0,0 +1,2 @@
+// StarLinkOptimiser/src/index.ts
+console.log("Hello from StarLinkOptimiser");
---
+++ StarLinkOptimiser/test/runTests.ts
@@ -0,0 +1,6 @@
+// StarLinkOptimiser/test/runTests.ts
+import { run } from '../../src/PrincipiaTest';
+import './index.test';
+// Import other test files here
+
+run();
