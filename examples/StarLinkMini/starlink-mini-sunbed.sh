6f019a7af006818ee0e971e5466b8b0a3a300187
Rick Hallett <rickhallett@icloud.com>
2025-07-01 23:42:09 +0100

    CHORE: Clean up examples directory
    - Remove redundant example files that were incorrectly tracked
    - Examples directory is now properly organized

--- examples/StarLinkMini/starlink-mini-sunbed.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# StarLinkMini cron job for session: sunbed
-cd /Users/oceanheart/nstcg-website/src/StarLinkMini && bun run /Users/oceanheart/nstcg-website/src/StarLinkMini/index.ts --name sunbed --single-run --direction 300 --tilt 40
--- examples/StarLinkOptimiser/test/index.test.ts
+++
@@ -1,8 +0,0 @@
-// StarLinkOptimiser/test/index.test.ts
-import { describe, it, expect } from '../../src/PrincipiaTest';
-
-describe('StarLinkOptimiser', () => {
-    it('should be true', () => {
-        expect(true).toBe(true);
-    });
-});
